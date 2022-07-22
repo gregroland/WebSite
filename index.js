c176c5df1878a81d7d905a0676a4e3f6dedf0b49
gregroland <greg.roland66@gmail.com>
2022-07-22 13:02:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function change(){
-  
+  let h1 = document.getElementById('title').innerHTML = 'great'
 }
 
 
-document.getElementById(
+let h1 = document.getElementById('title')
+h1.addEventListener('click', change)
